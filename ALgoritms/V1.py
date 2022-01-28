b09ea1a5cc447a09c45cd8f37e454f4490ecc043
daniluck505 <daniluck-505@yandex.ru>
2022-01-28 12:25:49 +0300

    v1_2

--- ALgoritms/V1.py
+++ ALgoritms/V1.py
@@ -1,21 +1,33 @@
-# class Solution:
-#     def searchInsert(self, nums: List[int], target: int) -> int:
-
-
-# for i, j in [([1,2 3,4], 5)]:
-#     print(searchInsert(nums=i, target=j))
-
+# -----------a1----------------
 
 def searchInsert(nums, target):
     for i, j in enumerate(nums):
-        if target < j:
-            if i > 0:
-                return i-1
-            else:
-                return -1
-        elif target == j:
-            return i
-        elif target > j
+        if target == j:
+            return(i)
+        elif target < j and i == 0:
+            return(0)
+        elif target < j+1:
+            return(i)
+    return(len(nums))
 
-# searchInsert([0, 1, 4], )
-print(range(-7, 8))
+
+n = searchInsert([0, 1, 3, 5], 6)
+
+# -----------a2----------------
+
+
+def strStr(haystack, needle):
+    if needle == '':
+        return 0
+    else:
+        for i, j in enumerate(haystack):
+            if len(haystack[i:]) < len(needle):
+                return -1
+            elif j == needle[0]:
+                if haystack[i:i+len(needle)] == needle:
+                    return i
+        return -1
+
+
+answer = strStr('hello', '')
+print(answer)
